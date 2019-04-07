d1c632385f80dbfc7b435d1f95ab69d96f83b171
sgma-svn <jbarua@gmail.com>
2019-04-07 09:34:19 -0500

    Update wbpwmaudio.v

--- wbpwmaudio.v
+++ wbpwmaudio.v
@@ -1,1 +1,4 @@
-
+// PWM controller specific to audio. 
+// Creates Pulse Width Modulated history - where the amount of 
+// time the output is high is determined by the pulse width 
+// data given to it.
